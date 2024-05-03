6d4a32887992779bc09061ba5879fcff4de0159a
Milan Sobat <milansobat03@gmail.com>
2024-05-03 15:00:16 -0700

    Refactor to add to dictionary values and do asserts

--- 100DayPlayground.playground/Pages/FizzBuzz.xcplaygroundpage/Contents.swift
+++ 100DayPlayground.playground/Pages/FizzBuzz.xcplaygroundpage/Contents.swift
@@ -2,21 +2,24 @@
 
 import Foundation
 
-var multiplesDictionary = ["multipleOf3": 0, "multipleOf5": 0, "multipleOfBoth": 0];
+var multiplesDictionary = ["Fizz": 0, "Buzz": 0, "FizzBuzz": 0, "other": 0];
 for i in 1...100 {
-    var multipleOf3 = i.isMultiple(of: 3) ? true : false
-    var multipleOf5 = i.isMultiple(of: 5) ? true : false
+    var multipleOf3 = i.isMultiple(of: 3)
+    var multipleOf5 = i.isMultiple(of: 5)
 
     if multipleOf3 && multipleOf5 {
-        print("FizzBuzz")
-    } else if !multipleOf3 && !multipleOf5 {
-        print(i)
-    } else if !multipleOf3 && multipleOf5 {
-        print("Buzz")
+        multiplesDictionary["FizzBuzz"]! += 1
+    } else if multipleOf3 {
+        multiplesDictionary["Fizz"]! += 1
+    } else if multipleOf5 {
+        multiplesDictionary["Buzz"]! += 1
     } else {
-        print("Fizz")
+        multiplesDictionary["other"]! += 1
     }
 }
 
 //: [Next](@next)
-
+assert(multiplesDictionary["Fizz"] == 27)
+assert(multiplesDictionary["Buzz"] == 14)
+assert(multiplesDictionary["FizzBuzz"] == 6)
+assert((100 - (multiplesDictionary["Fizz"]! + multiplesDictionary["Buzz"]! + multiplesDictionary["FizzBuzz"]!)) == multiplesDictionary["other"]!)
